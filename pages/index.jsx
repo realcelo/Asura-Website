0fcc2cd6eb2fbe6e5ed137f772b6d9f3620d5b44
Celo <66029983+realcelo@users.noreply.github.com>
2022-12-20 05:35:03 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,19 @@ Vote Asura</a>
                   <p className="text-3xl text-white font-semibold">Level-Up Your Server!</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Asura, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://i.imgur.com/JfC6fK0.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://i.imgur.com/gJSDi9A.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+              </>
+      
+        </div>
+		  
+		  <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
+              <>
+                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+                  <img src="https://i.imgur.com/JfC6fK0.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <p className="text-3xl text-white font-semibold">Level-Up Your Server!</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Asura, the best and most up-to-date bot?</p>
+                </div>
+                <img src="https://i.imgur.com/0v3qxtb.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
